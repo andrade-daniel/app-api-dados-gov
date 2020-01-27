cc371ac3f519572cfed8301f16d48392eddd8e03
Daniel Andrade <daniel.andrade@ama.pt>
2020-01-27 14:37:23 +0000

    removing download function

--- app.py
+++ app.py
@@ -22,26 +22,6 @@ def add_api_key(api_key):
 
     return api_key
 
-# @st.cache(persist=True, suppress_st_warning=True)
-def download(url: str, dest_folder: str):
-    if not os.path.exists(dest_folder):
-        os.makedirs(dest_folder)  # create folder if it does not exist
-
-    filename = url.split("/")[-1].replace(" ", "_")  # be careful with file names
-    file_path = os.path.join(dest_folder, filename)
-
-    r = requests.get(url, stream=True)
-    if r.ok:
-        print("saving to", os.path.abspath(file_path))
-        with open(file_path, "wb") as f:
-            for chunk in r.iter_content(chunk_size=1024 * 8):
-                if chunk:
-                    f.write(chunk)
-                    f.flush()
-                    os.fsync(f.fileno())
-    else:  # HTTP status code 4XX/5XX
-        print("Download failed: status code {}\n{}".format(r.status_code, r.text))
-
 
 # logo image
 
